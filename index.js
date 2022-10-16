c0b5bd15c4175fc56363e3402f64ff5e77b5f578
Christopher Dowdy <cdowdy81@gmail.com>
2022-10-16 19:36:53 -0400

    Push for heroku

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
